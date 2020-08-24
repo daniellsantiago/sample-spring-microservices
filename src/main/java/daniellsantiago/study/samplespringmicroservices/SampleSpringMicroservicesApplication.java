653a2dd679b8377cf4cc66cf1c1153eecf0f77c8
Daniel Santiago <danielsantiago111@hotmail.com>
2020-08-24 11:45:01 -0300

    Initial project structure

---
+++ src/main/java/daniellsantiago/study/samplespringmicroservices/SampleSpringMicroservicesApplication.java
@@ -0,0 +1,13 @@
+package daniellsantiago.study.samplespringmicroservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleSpringMicroservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleSpringMicroservicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/daniellsantiago/study/samplespringmicroservices/SampleSpringMicroservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package daniellsantiago.study.samplespringmicroservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleSpringMicroservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
