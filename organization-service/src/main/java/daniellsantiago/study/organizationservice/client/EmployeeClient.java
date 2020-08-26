895e2c530b6768a1e7e41ff2801bb2ee43780b1c
Daniel Santiago <danielsantiago111@hotmail.com>
2020-08-26 09:30:00 -0300

    Add Feign Client to communicate Employee service with Organization service

---
+++ organization-service/src/main/java/daniellsantiago/study/organizationservice/client/EmployeeClient.java
@@ -0,0 +1,15 @@
+package daniellsantiago.study.organizationservice.client;
+
+import daniellsantaigo.study.commonclasses.model.Employee;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
+
+@FeignClient(name = "employee-service")
+public interface EmployeeClient {
+
+    @GetMapping("/organization/{organizationId}")
+    List<Employee> findByOrganization(@PathVariable("organizationId") Long organizationId);
+}
