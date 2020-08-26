147639405ca24bd07d59b7285de1209800cb001f
Daniel Santiago <danielsantiago111@hotmail.com>
2020-08-26 09:30:19 -0300

    Add Employee controller to handle api requests

---
+++ employee-service/src/main/java/daniellsantiago/study/employeeservice/controller/EmployeeController.java
@@ -0,0 +1,48 @@
+package daniellsantiago.study.employeeservice.controller;
+
+import daniellsantaigo.study.commonclasses.model.Employee;
+import daniellsantaigo.study.commonclasses.repository.EmployeeRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
+@Slf4j
+public class EmployeeController {
+
+    private final EmployeeRepository repository;
+
+    @PostMapping("/")
+    public Employee add(@RequestBody Employee employee) {
+        log.info("Employee add: {}", employee);
+        return repository.add(employee);
+    }
+
+    @GetMapping("/{id}")
+    public Employee findById(@PathVariable("id") Long id) {
+        log.info("Employee find by id={}", id);
+        return repository.findById(id);
+    }
+
+    @GetMapping("/")
+    public List<Employee> findAll() {
+        log.info("Employee find all");
+        return repository.findAll();
+    }
+
+    @GetMapping("/department/{departmentId}")
+    public List<Employee> findByDepartment(@PathVariable("departmentId") Long departmentId) {
+        log.info("Employee find by departmentId={}", departmentId);
+        return repository.findByDepartment(departmentId);
+    }
+
+    @GetMapping("/organization/{organizationId}")
+    public List<Employee> findByOrganization(@PathVariable("organizationId") Long organizationId) {
+        log.info("Employee find by organizationId={}", organizationId);
+        return repository.findByOrganization(organizationId);
+    }
+}
