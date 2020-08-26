d59cbeb27c0075166b7b7181fee18d98bf359119
Daniel Santiago <danielsantiago111@hotmail.com>
2020-08-26 09:10:37 -0300

    Add department model and Repository to common classes

---
+++ common-classes/src/main/java/daniellsantaigo/study/commonclasses/model/Department.java
@@ -0,0 +1,24 @@
+package daniellsantaigo.study.commonclasses.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Department {
+    private Long id;
+    private Long organizationId;
+    private String name;
+    private List<Employee> employees = new ArrayList<>();
+
+    public Department(Long organizationId, String name) {
+        super();
+        this.organizationId = organizationId;
+        this.name = name;
+    }
+}
