eec6fb5a037db26402e88be1a4e649d220ed0465
Daniel Santiago <danielsantiago111@hotmail.com>
2020-08-26 09:14:01 -0300

    Add Feign Client to communicate Department microservice to Organization microservice

---
+++ organization-service/src/main/java/daniellsantiago/study/organizationservice/client/DepartmentClient.java
@@ -0,0 +1,18 @@
+package daniellsantiago.study.organizationservice.client;
+
+import daniellsantaigo.study.commonclasses.model.Department;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
+
+@FeignClient(name = "department-service")
+public interface DepartmentClient {
+
+    @GetMapping("/organization/{organizationId}")
+    List<Department> findByOrganization(@PathVariable("organizationId") Long organizationId);
+
+    @GetMapping("/organization/{organizationId}/with-employees")
+    List<Department> findByOrganizationWithEmployees(@PathVariable("organizationId") Long organizationId);
+}
