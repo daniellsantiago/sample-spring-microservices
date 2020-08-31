bda03645cd28595942703e8129c67cb847750155
Daniel Santiago <danielsantiago111@hotmail.com>
2020-08-31 13:48:43 -0300

    Changed routing from property annotation to Java Route API

--- gateway-service/src/main/java/daniellsantiago/study/gatewayservice/GatewayServiceApplication.java
+++ gateway-service/src/main/java/daniellsantiago/study/gatewayservice/GatewayServiceApplication.java
@@ -2,8 +2,10 @@ package daniellsantiago.study.gatewayservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -11,6 +13,21 @@ public class GatewayServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayServiceApplication.class, args);
+	}
+
+	@Bean
+	public RouteLocator myRoutes(RouteLocatorBuilder builder) {
+		return builder.routes()
+				.route("employee-service", p -> p
+					.path("/employee/**")
+					.uri("lb://employee-service"))
+				.route("department-service", p -> p
+					.path("/department/**")
+					.uri("lb://department-service"))
+				.route("organization-service", p -> p
+					.path("/organization/**")
+					.uri("lb://organization-service"))
+				.build();
 	}
 
 }
