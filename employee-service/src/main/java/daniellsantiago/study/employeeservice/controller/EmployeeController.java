0e5be8aa21b1e873097dad67c1f74ff10bb9ae14
Daniel Santiago <danielsantiago111@hotmail.com>
2020-08-31 21:31:55 -0300

    Added methods to test hystrix fallback setted by gateway

--- employee-service/src/main/java/daniellsantiago/study/employeeservice/controller/EmployeeController.java
+++ employee-service/src/main/java/daniellsantiago/study/employeeservice/controller/EmployeeController.java
@@ -7,6 +7,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.Duration;
 import java.util.List;
 
 @RestController
@@ -45,5 +46,17 @@ public class EmployeeController {
     public List<Employee> findByOrganization(@PathVariable("organizationId") Long organizationId) {
         log.info("Employee find by organizationId={}", organizationId);
         return repository.findByOrganization(organizationId);
+    }
+
+    @GetMapping("/willfail")
+    public void failRequest() throws InterruptedException {
+        log.info("Calling a method that will fail");
+        Thread.sleep(Duration.ofSeconds(3).toMillis());
+    }
+
+    @GetMapping("/fallback")
+    public String fallback(){
+        log.info("Fallback method");
+        return "Imagine something useful being done to continue the application flow";
     }
 }
