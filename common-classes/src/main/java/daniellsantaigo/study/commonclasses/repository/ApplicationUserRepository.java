a59025d06fa51b8c5f74467974409f13b7a66462
Daniel Santiago <danielsantiago111@hotmail.com>
2020-09-04 22:03:28 -0300

    Repository to manipulate user info

---
+++ common-classes/src/main/java/daniellsantaigo/study/commonclasses/repository/ApplicationUserRepository.java
@@ -0,0 +1,19 @@
+package daniellsantaigo.study.commonclasses.repository;
+
+import daniellsantaigo.study.commonclasses.model.ApplicationUser;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ApplicationUserRepository {
+    private final List<ApplicationUser> users = new ArrayList<>();
+
+    public ApplicationUser findByUsername(String username) {
+        return users.stream()
+                .filter(applicationUser -> applicationUser.getUsername().equalsIgnoreCase(username)).findFirst().get();
+    }
+
+    public void add(ApplicationUser user) {
+        users.add(user);
+    }
+}
