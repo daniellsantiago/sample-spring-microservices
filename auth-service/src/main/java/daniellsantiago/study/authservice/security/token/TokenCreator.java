b9a97c1b19b9333722e99ba7dce111e147013744
Daniel Santiago <danielsantiago111@hotmail.com>
2020-09-04 22:05:09 -0300

    Handle Token jwt

---
+++ auth-service/src/main/java/daniellsantiago/study/authservice/security/token/TokenCreator.java
@@ -0,0 +1,89 @@
+package daniellsantiago.study.authservice.security.token;
+
+import com.nimbusds.jose.*;
+import com.nimbusds.jose.crypto.DirectEncrypter;
+import com.nimbusds.jose.crypto.RSASSASigner;
+import com.nimbusds.jose.jwk.JWK;
+import com.nimbusds.jose.jwk.RSAKey;
+import com.nimbusds.jwt.JWTClaimsSet;
+import com.nimbusds.jwt.SignedJWT;
+import daniellsantaigo.study.commonclasses.model.ApplicationUser;
+import daniellsantaigo.study.commonclasses.property.JwtConfiguration;
+import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.stereotype.Service;
+
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.interfaces.RSAPublicKey;
+import java.util.Date;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class TokenCreator {
+    private final JwtConfiguration jwtConfiguration;
+
+    @SneakyThrows
+    public SignedJWT createSignedJwt(Authentication auth) {
+        ApplicationUser user = (ApplicationUser) auth.getPrincipal();
+        JWTClaimsSet jwtClaimSet = createJWTClaimSet(auth, user);
+
+        KeyPair keyPair = generateKeyPair();
+
+        JWK jwk = new RSAKey.Builder((RSAPublicKey) keyPair.getPublic()).keyID(UUID.randomUUID().toString()).build();
+
+        SignedJWT signedJWT = new SignedJWT(new JWSHeader.Builder(JWSAlgorithm.RS256)
+                .jwk(jwk)
+                .type(JOSEObjectType.JWT)
+                .build(), jwtClaimSet);
+
+        RSASSASigner signer = new RSASSASigner(keyPair.getPrivate());
+
+        signedJWT.sign(signer);
+
+        log.info("Serialized token '{}'", signedJWT.serialize());
+
+        return signedJWT;
+    }
+
+    private JWTClaimsSet createJWTClaimSet(Authentication auth, ApplicationUser user) {
+        return new JWTClaimsSet.Builder()
+                .subject(user.getUsername())
+                .claim("authorities", auth.getAuthorities()
+                        .stream()
+                        .map(GrantedAuthority::getAuthority)
+                        .collect(Collectors.toList()))
+                .claim("userId", user.getId())
+                .issuer("https://github.com/daniellsantiago")
+                .issueTime(new Date())
+                .expirationTime(new Date(System.currentTimeMillis() + (jwtConfiguration.getExpiration() * 1000))).build();
+    }
+
+    @SneakyThrows
+    private KeyPair generateKeyPair() {
+        KeyPairGenerator generator = KeyPairGenerator.getInstance("RSA");
+
+        generator.initialize(2048);
+
+        return generator.genKeyPair();
+    }
+
+    @SneakyThrows
+    public String encryptToken(SignedJWT signedJWT) {
+        DirectEncrypter directEncrypter = new DirectEncrypter(jwtConfiguration.getPrivateKey().getBytes());
+
+        JWEObject jweObject = new JWEObject(new JWEHeader.Builder(JWEAlgorithm.DIR, EncryptionMethod.A128CBC_HS256)
+                .contentType("JWT")
+                .build(), new Payload(signedJWT));
+
+        jweObject.encrypt(directEncrypter);
+
+        return jweObject.serialize();
+    }
+}
