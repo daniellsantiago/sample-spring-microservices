53cb52323515ead5d917fb730b5b9864f4885249
Daniel Santiago <danielsantiago111@hotmail.com>
2020-09-07 18:55:33 -0300

    Add @ComponentScan to Gateway service visualize common classes

--- gateway-service/src/main/java/daniellsantiago/study/gatewayservice/GatewayServiceApplication.java
+++ gateway-service/src/main/java/daniellsantiago/study/gatewayservice/GatewayServiceApplication.java
@@ -6,9 +6,11 @@ import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EnableEurekaClient
+@ComponentScan("daniellsantiago.study")
 public class GatewayServiceApplication {
 
 	public static void main(String[] args) {
